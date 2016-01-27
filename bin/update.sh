3eaa1036371760826632d57ade46503c9494b4db
Nicco Kunzmann <niccokunzmann@googlemail.com>
2016-01-27 17:37:39 +0100

    Create update.sh

---
+++ bin/update.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd "`dirname $0`"
+
+cd ../web
+
+rm -r downloads
+mkdir downloads
+cd downloads
+
+# Sublime
+wget http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.2.dmg
+wget http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.2%20x64.tar.bz2
+wget http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.2.zip
+wget http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.2%20Setup.exe
+
+# Notepad++
+wget https://notepad-plus-plus.org/repository/6.x/6.8.3/npp.6.8.3.Installer.exe
