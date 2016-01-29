f01f8b40405b07dc0dce49457736cc363fe2d462
Nicco Kunzmann <niccokunzmann@rambler.ru>
2016-01-29 15:13:39 +0000

    downloading only if newer

--- bin/update.sh
+++ bin/update.sh
@@ -5,18 +5,20 @@ cd "`dirname $0`"
 mkdir -p ../web
 cd ../web
 
-rm -r downloads
-mkdir downloads
+mkdir -p downloads
 cd downloads
 
+# wget -c -N
+#   http://superuser.com/questions/283481/how-do-i-properly-set-wget-to-download-only-new-files#283484
+
 # Sublime
-wget http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.2.dmg
-wget http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.2%20x64.tar.bz2
-wget http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.2.zip
-wget http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.2%20Setup.exe
+wget -c -N http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.2.dmg
+wget -c -N http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.2%20x64.tar.bz2
+wget -c -N http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.2.zip
+wget -c -N http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.2%20Setup.exe
 
 # Notepad++
-wget https://notepad-plus-plus.org/repository/6.x/6.8.3/npp.6.8.3.Installer.exe
+wget -c -N https://notepad-plus-plus.org/repository/6.x/6.8.3/npp.6.8.3.Installer.exe
 
 # PyCharm
 # TODO
